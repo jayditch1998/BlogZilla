55bc89363288b3d016cb745c65ca4309e9a11232
jayditch1998 <balansijayditch@gmail.com>
2021-08-01 21:54:32 -0700

    test update git

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>BlogZilla</title>
+        <title>BlogZilla test</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
