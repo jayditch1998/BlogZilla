83387d86d5a0c2621af0748a4d8605a5448ee7fb
jayditch1998 <balansijayditch@gmail.com>
2021-08-02 22:34:53 -0700

    update react

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
-    //
+    public function get(){
+        return Post::get();
+    }
+    public function delete($id){
+        return response()->json(Post::destroy($id));
+    }
+    public function put(Request $request,$id){
+        $post = Post::where('id', $id)->first();
+        $post->title = $request->title;
+        $post->body =$request->body;
+        $post->save();
+        return response()->json($post);
+    }
+    public function post(Request $request){
+        $post = new Post();
+        $post->title = $request->title;
+        $post->body =$request->body;
+        $post->save();
+        return response()->json($post);
+    }
 }
